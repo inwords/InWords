412736ae88e38a27b6b38c575debb441227662c5
anzer987 <anzer987@yandex.ru>
2018-09-08 13:04:55 +0300

    Switched to a new branch

--- WebApi/InWords.WebApi/InWords.WebApi/Controllers/ValuesController.cs
+++ WebApi/InWords.WebApi/InWords.WebApi/Controllers/ValuesController.cs
@@ -21,7 +21,7 @@ namespace InWords.WebApi.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "value";
+            return $"value:{id}";
         }
 
         // POST api/values
