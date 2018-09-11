e50808b48c2a7f488e3cf3fbdf3207a7d5dd214d
anzer987 <anzer987@yandex.ru>
2018-09-11 21:47:27 +0300

    auth develop-1

---
+++ WebApi/InWords.WebApi/InWords.Auth/Models/RsaKeyProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InWords.Auth
+{
+    public class RsaKeyProvider
+    {
+    }
+}
---
+++ WebApi/InWords.WebApi/InWords.WebApi/Controllers/AuthController.cs
@@ -0,0 +1,47 @@
+﻿namespace InWords.WebApi.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IdentityModel.Tokens.Jwt;
+    using System.Linq;
+    using System.Security.Claims;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.IdentityModel.Tokens;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("token")]
+        public async Task<IActionResult> Token()
+        {
+            //string tokenString = "test";
+            var header = Request.Headers["Authorization"];
+            if (header.ToString().StartsWith("Basic"))
+            {
+                var credValue = header.ToString().Substring("Basic ".Length).Trim();
+                var usernameAndPassenc = Encoding.UTF8.GetString(Convert.FromBase64String(credValue)); //admin:pass
+                var usernameAndPass = usernameAndPassenc.Split(":");
+                //check in DB username and pass exist
+
+                if (usernameAndPass[0] == "Admin" && usernameAndPass[1] == "pass")
+                {
+                    var claimsdata = new[] { new Claim(ClaimTypes.Name, usernameAndPass[0]) };
+                    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("ahbasshfbsahjfbshajbfhjasbfashjbfsajhfvashjfashfbsahfbsahfksdjf"));
+                    var signInCred = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+                    var token = new JwtSecurityToken(
+                         expires: DateTime.Now.AddMinutes(1),
+                         claims: claimsdata,
+                         signingCredentials: signInCred
+                        );
+                    var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+                    return Ok(tokenString);
+                }
+            }
+            return BadRequest("wrong request");
+        }
+    }
+}
