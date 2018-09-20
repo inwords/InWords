53b36a0ce08b784dbf5a20d2a00f676ce3ccd9ae
anzer987 <anzer987@yandex.ru>
2018-09-20 22:18:29 +0300

    data model update v2

--- WebApi/InWords.WebApi/InWords.Data/InWordsDataContext.cs
+++ WebApi/InWords.WebApi/InWords.Data/InWordsDataContext.cs
@@ -25,6 +25,20 @@
 
         public DbSet<Account> Accounts { get; set; }
 
+        public DbSet<Language> Languages { get; set; }
+
+        public DbSet<Word> Words { get; set; }
+
+        public DbSet<WordPair> WordPairs { get; set; }
+
+        public DbSet<UserWordPair> UserWordPairs { get; set; }
+
+        public DbSet<Seria> Series { get; set; }
+
+        public DbSet<SeriaWord> SeriaWords { get; set; }
+
+        public DbSet<UserSeria> UsersSerias { get; set; }
+
         private void RecreateDb()
         {
 
@@ -33,7 +47,7 @@
                 _created = true;
                 Database.EnsureDeleted();
                 Database.EnsureCreated();
-                Accounts.Add(new Account() { Email = "admin@gmail.com", Password = "1234", Role = RoleType.Admin,RegistrationData = DateTime.Now});
+                Accounts.Add(new Account() { Email = "admin@gmail.com", Password = "1234", Role = RoleType.Admin, RegistrationData = DateTime.Now });
                 Accounts.Add(new Account() { Email = "user@gmail.com", Password = "1234", Role = RoleType.User, RegistrationData = DateTime.Now });
                 SaveChanges();
             }
