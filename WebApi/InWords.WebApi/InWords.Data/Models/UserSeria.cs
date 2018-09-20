8eb898f20f7db32c803ca94ea57df535091270b0
anzer987 <anzer987@yandex.ru>
2018-09-20 22:21:55 +0300

    model bug fix

--- WebApi/InWords.WebApi/InWords.Data/Models/UserSeria.cs
+++ WebApi/InWords.WebApi/InWords.Data/Models/UserSeria.cs
@@ -6,7 +6,7 @@
 
     public class UserSeria
     {
-        public int UserSeriesID { get; set; }
+        public int UserSeriaID { get; set; }
 
         public virtual Seria Seria { get; set; }
 
