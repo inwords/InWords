a4a376483ba737c1ab1843ffd3d78df10868941b
Rugged-bl <vasia.shumilov@gmail.com>
2018-10-29 21:01:07 +0300

    Cleanup

--- Android/app/src/main/java/com/dreamproject/inwords/BasicModelPresenter.java
+++
@@ -1,9 +0,0 @@
-package com.dreamproject.inwords;
-
-public class BasicModelPresenter<Model> extends BasicPresenter {
-    protected Model model;
-
-    public BasicModelPresenter(Model model) {
-        this.model = model;
-    }
-}
