a86a523c408d99d53d9e923cdb56cde59d8a3dc5
Alword <anzer987@yandex.ru>
2018-10-29 21:27:49 +0300

    logger

---
+++ WebApi/InWords.WebApi/InWords.WebApi/Providers/FIleLogger/FileLoggerProvider.cs
@@ -0,0 +1,21 @@
+﻿namespace InWords.WebApi.Providers
+{
+    using Microsoft.Extensions.Logging;
+
+    public class FileLoggerProvider : ILoggerProvider
+    {
+        private string path;
+        public FileLoggerProvider(string _path)
+        {
+            path = _path;
+        }
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new FileLogger(path);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
