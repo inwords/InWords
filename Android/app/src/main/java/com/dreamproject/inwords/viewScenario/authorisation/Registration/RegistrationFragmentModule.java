330d7c6638156c319d22709ae61ac7ab5cc1257e
Rugged-bl <vasia.shumilov@gmail.com>
2018-11-02 19:10:20 +0300

    Game levels fragment added to navigation

--- Android/app/src/main/java/com/dreamproject/inwords/viewScenario/authorisation/Registration/RegistrationFragmentModule.java
+++ Android/app/src/main/java/com/dreamproject/inwords/viewScenario/authorisation/Registration/RegistrationFragmentModule.java
@@ -5,7 +5,7 @@ import dagger.Module;
 import dagger.android.ContributesAndroidInjector;
 
 @Module(subcomponents = RegistrationFragmentComponent.class)
-public abstract class RegistrationModule {
+public abstract class RegistrationFragmentModule {
     @ContributesAndroidInjector(modules = { /* modules to install into the subcomponent */ })
     abstract RegistrationFragment contributeYourActivityInjector();
 }
