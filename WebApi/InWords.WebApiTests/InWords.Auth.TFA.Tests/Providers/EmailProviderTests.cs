c07f0cdc871e3bef6613201579ac06d370790032
Alword <anzer987@yandex.ru>
2018-11-16 22:42:25 +0300

    test 2FA passed rename

---
+++ WebApi/InWords.WebApiTests/InWords.Auth.TFA.Tests/Providers/EmailProviderTests.cs
@@ -0,0 +1,17 @@
+﻿namespace InWords.Auth.TFA.Tests
+{
+    using InWords.Auth.TFA.Providers;
+    using System;
+    using Xunit;
+
+    public class EmailProviderTests
+    {
+        [Fact]
+        public void SendMailTest1()
+        {
+            var EmailProvider = new EmailProvider();
+
+            EmailProvider.SendMail();
+        }
+    }
+}
---
+++ WebApi/InWords.WebApiTests/InWords.Auth.TFA.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace InWords.Auth.TFA.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
