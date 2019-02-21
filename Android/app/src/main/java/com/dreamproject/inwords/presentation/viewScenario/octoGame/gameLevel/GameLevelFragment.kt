e1267775705452dffc16c0ee39cd4cfa4f221f1c
Rugged-bl <vasia.shumilov@gmail.com>
2019-02-21 15:43:54 +0300

    Card words added

--- Android/app/src/main/java/com/dreamproject/inwords/presentation/viewScenario/octoGame/gameLevel/GameLevelFragment.kt
+++ Android/app/src/main/java/com/dreamproject/inwords/presentation/viewScenario/octoGame/gameLevel/GameLevelFragment.kt
@@ -4,11 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TableRow
 import androidx.fragment.app.Fragment
 import com.dreamproject.inwords.R
+import com.dreamproject.inwords.data.dto.WordTranslation
 import com.dreamproject.inwords.domain.BundleKeys
 import com.dreamproject.inwords.domain.GameLevelInfo
+import kotlinx.android.synthetic.main.card_front.view.*
 import kotlinx.android.synthetic.main.fragment_game_level.*
+import java.util.*
+
 
 class GameLevelFragment : Fragment() {
     private lateinit var gameLevelInfo: GameLevelInfo
@@ -29,5 +34,40 @@ class GameLevelFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         text.text = gameLevelInfo.toString()
+
+        val wordTranslations: List<WordTranslation> =
+                listOf(WordTranslation("car", "мошниа"),
+                        WordTranslation("box", "каропка"))
+
+        val words = prepairWordsForCards(wordTranslations)
+
+        val rows = words.size / 2
+        for (i in 0 until rows) {
+            val tableRow = TableRow(context)
+            tableRow.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT)
+
+            for (j in 0 until 2) {
+                val card = layoutInflater.inflate(R.layout.card, tableRow, false)
+                card
+                card.frontText.text = words[j + i * 2]
+
+                tableRow.addView(card, j)
+            }
+
+            table.addView(tableRow, i)
+        }
+    }
+
+    private fun prepairWordsForCards(wordTranslations: List<WordTranslation>): List<String> {
+        val words = ArrayList<String>(wordTranslations.size)
+
+        for (wordTranslation in wordTranslations) {
+            words.add(wordTranslation.wordForeign)
+            words.add(wordTranslation.wordNative)
+        }
+        words.shuffle()
+
+        return words;
     }
 }
