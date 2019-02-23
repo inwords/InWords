ec06cfea857c9d768f1b1c586db8d54e2c6b2dd7
Alword <anzer987@yandex.ru>
2019-02-23 13:11:30 +0300

    initial creation repository

---
+++ WebApi/InWords.WebApi/InWords.Data/Models/InWords/Repositories/CreationsRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace InWords.Data.Models
+{
+    using System.Linq;
+
+    public class CreationsRepository : Repository<Creation>
+    {
+        InWordsDataContext context = null;
+        public CreationsRepository(InWordsDataContext context) : base(context) { };
+    }
+}
--- WebApi/InWords.WebApi/InWords.Data/Models/InWords/Repositories/UserRepository.cs
+++ WebApi/InWords.WebApi/InWords.Data/Models/InWords/Repositories/UserRepository.cs
@@ -4,7 +4,6 @@
 
     public class UserRepository : Repository<User>
     {
-        InWordsDataContext context;
         public UserRepository(InWordsDataContext context) : base(context)
         {
             this.context = context;
