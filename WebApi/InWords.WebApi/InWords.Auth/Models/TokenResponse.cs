477bad904ed9e2ae68a3351246caff21e43b8681
Артём <a.a.slepushko@outlook.com>
2019-04-09 23:56:36 +0300

    update token response data

--- WebApi/InWords.WebApi/InWords.Auth/Models/TokenResponse.cs
+++ WebApi/InWords.WebApi/InWords.Auth/Models/TokenResponse.cs
@@ -7,19 +7,15 @@ namespace InWords.Auth.Models
 {
     public class TokenResponse
     {
+        public string Token { get; }
+
+        public int UserId { get; }
+
         public TokenResponse(ClaimsIdentity identity)
         {
-            // receive token
             string encodedJwt = AuthOptions.TokenProvider.GenerateToken(identity);
-
-
-
             Token = Cheeked(encodedJwt, out string errMsg);
             UserId = identity.Claims.GetUserId();
-
-            // TODO remove
-            Access_token = Token;
-            Email = "Method1 is deprecated, please use Method2 instead, pls use UserId instead";
         }
 
         private static string Cheeked(string encodedJwt, out string errorMsg)
@@ -28,18 +24,5 @@ namespace InWords.Auth.Models
             if (encodedJwt == null) errorMsg = "Token fail";
             return encodedJwt;
         }
-
-        #region Props
-
-        [Obsolete] // TODO remove depricated
-        public string Access_token { get; }
-        [Obsolete]
-        public string Email { get; }
-
-        public string Token { get; }
-
-        public int UserId { get; }
-
-        #endregion
     }
 }
