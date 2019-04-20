ffb5f79e30215bf163426d3db1a6304b2b43f0dd
Alword <anzer987@yandex.ru>
2019-04-20 20:23:56 +0300

    still build btw

--- WebApi/InWords.WebApi/InWords.WebApi/AppStart/IocRegister.cs
+++ WebApi/InWords.WebApi/InWords.WebApi/AppStart/IocRegister.cs
@@ -4,6 +4,8 @@ using InWords.Data.Models;
 using System;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using InWords.Data;
 
 namespace InWords.WebApi.AppStart
 {
@@ -14,7 +16,18 @@ namespace InWords.WebApi.AppStart
             var builder = new ContainerBuilder();
             builder.Populate(services);
 
+            // register context
             builder.Register(_ => new InWordsDataContext(Configuration.GetConnectionString("DefaultConnection"))).InstancePerLifetimeScope();
+
+
+            //register repositories
+            var repositoryAssembly = Assembly.GetAssembly(typeof(InWordsDataContext));
+            builder.RegisterAssemblyTypes(repositoryAssembly)
+                .Where(a => a.Name.EndsWith("Repository") && a.Name.StartsWith("InWords.Data")).InstancePerLifetimeScope();
+
+            //register services
+            builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
+                .Where(a => a.Name.EndsWith("Service") && a.Name.StartsWith("InWords.WebApi.Services")).InstancePerLifetimeScope();
 
             var container = builder.Build();
             return new AutofacServiceProvider(container);
