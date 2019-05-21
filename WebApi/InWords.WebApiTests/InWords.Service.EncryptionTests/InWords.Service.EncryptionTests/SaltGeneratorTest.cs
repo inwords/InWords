8a01eed2c7e6fb1a80a6f44173a1e05eada28c78
Alword <anzer987@yandex.ru>
2019-05-21 19:15:55 +0300

    Encription tests

---
+++ WebApi/InWords.WebApiTests/InWords.Service.EncryptionTests/InWords.Service.EncryptionTests/SaltGeneratorTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using InWords.Service.Encryption;
+using Xunit;
+
+namespace InWords.Service.EncryptionTests
+{
+    public class SaltGeneratorTestSaltGeneratorTest
+    {
+        [Theory]
+        [InlineData("123")]
+        [InlineData("パスワード")]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void EqualsSequence(string password)
+        {
+            var saltGenerator = new SaltGenerator();
+            byte[] key = saltGenerator.SaltPassword(password);
+            bool result = saltGenerator.EqualsSequence(password, key);
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void EqualsSequenceNull()
+        {
+            var saltGenerator = new SaltGenerator();
+            Assert.Throws<ArgumentNullException>(() => saltGenerator.SaltPassword(null));
+        }
+    }
+}
