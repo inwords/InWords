6a45b5eb3884ac740d772f9be5e36206c403fa3f
Alword <anzer987@yandex.ru>
2019-05-21 19:18:10 +0300

    salt tests

--- WebApi/InWords.WebApiTests/InWords.Service.EncryptionTests/InWords.Service.EncryptionTests/SaltGeneratorTest.cs
+++ WebApi/InWords.WebApiTests/InWords.Service.EncryptionTests/InWords.Service.EncryptionTests/SaltGeneratorTest.cs
@@ -25,5 +25,15 @@ namespace InWords.Service.EncryptionTests
             var saltGenerator = new SaltGenerator();
             Assert.Throws<ArgumentNullException>(() => saltGenerator.SaltPassword(null));
         }
+
+        [Fact]
+        public void EqualsSequenceBig()
+        {
+            string password = new string(';', 2048);
+            var saltGenerator = new SaltGenerator();
+            byte[] key = saltGenerator.SaltPassword(password);
+            bool result = saltGenerator.EqualsSequence(password, key);
+            Assert.True(result);
+        }
     }
 }
