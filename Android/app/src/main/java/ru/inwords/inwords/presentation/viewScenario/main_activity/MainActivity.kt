dd3a1753c8763ab1ed6bcc4ecb37b63bba13d686
Rugged-bl <vasia.shumilov@gmail.com>
2019-05-24 20:09:46 +0300

    added bottom padding in dictionary and bottom bar hidden when keyboard is open fixed #41

--- Android/app/src/main/java/ru/inwords/inwords/presentation/viewScenario/main_activity/MainActivity.kt
+++ Android/app/src/main/java/ru/inwords/inwords/presentation/viewScenario/main_activity/MainActivity.kt
@@ -2,6 +2,7 @@ package ru.inwords.inwords.presentation.viewScenario.main_activity
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
@@ -9,8 +10,11 @@ import androidx.navigation.ui.NavigationUI
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.GoogleApiAvailability
 import dagger.android.support.DaggerAppCompatActivity
+import io.reactivex.disposables.Disposable
 import kotlinx.android.synthetic.main.activity_main.*
 import ru.inwords.inwords.R
+import ru.inwords.inwords.core.KeyboardManager
+import ru.inwords.inwords.core.KeyboardStatus
 import javax.inject.Inject
 
 class MainActivity : DaggerAppCompatActivity() {
@@ -19,6 +23,9 @@ class MainActivity : DaggerAppCompatActivity() {
     private lateinit var viewModel: IntegrationViewModel
 
     private lateinit var navController: NavController
+
+    private var keyboardStatusDisposable: Disposable? = null
+    private var stoppedKeyboardListener = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +38,38 @@ class MainActivity : DaggerAppCompatActivity() {
         navController = Navigation.findNavController(this, R.id.main_nav_host_fragment)
 
         setupBottomNavMenu(navController)
+
+        keyboardStatusDisposable = KeyboardManager(this).status()
+                .subscribe {
+                    if (!stoppedKeyboardListener) {
+                        @Suppress("WHEN_ENUM_CAN_BE_NULL_IN_JAVA")
+                        when (it) {
+                            KeyboardStatus.OPEN -> hideNavigation(true)
+                            KeyboardStatus.CLOSED -> showNavigation(true)
+                        }
+                    }
+                }
+    }
+
+    override fun onDestroy() {
+        keyboardStatusDisposable?.dispose()
+        super.onDestroy()
+    }
+
+    private fun showNavigation(keyboardReferer: Boolean = false) {
+        navigation.visibility = View.VISIBLE
+
+        if (!keyboardReferer) {
+            stoppedKeyboardListener = false
+        }
+    }
+
+    private fun hideNavigation(keyboardReferer: Boolean = false) {
+        navigation.visibility = View.GONE
+
+        if (!keyboardReferer) {
+            stoppedKeyboardListener = true
+        }
     }
 
     private fun setupBottomNavMenu(navController: NavController) {
