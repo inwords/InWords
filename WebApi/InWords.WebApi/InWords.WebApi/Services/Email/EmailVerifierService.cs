ae822a7d0d625a15313df1e754cf806bdc51d2e4
Alword <anzer987@yandex.ru>
2019-08-29 21:31:43 +0300

    feature timeout exception

--- WebApi/InWords.WebApi/InWords.WebApi/Services/Email/EmailVerifierService.cs
+++ WebApi/InWords.WebApi/InWords.WebApi/Services/Email/EmailVerifierService.cs
@@ -1,4 +1,5 @@
 ﻿using InWords.Data.Domains;
+using InWords.Data.Domains.EmailEntitys;
 using InWords.Data.Repositories;
 using System;
 using System.Collections.Generic;
@@ -9,26 +10,41 @@ namespace InWords.WebApi.Services.Email
 {
     public class EmailVerifierService
     {
+        private const int EMAIL_TIMEOUT = 2; // MINUTES;
         private readonly EmailCodeGeneratorService codeGenerator = null;
         private readonly EmailSender emailSender = null;
-        private readonly EmailVerifierRepository emailVerifier = null;
+        private readonly EmailVerifierRepository emailVerifierRepository = null;
 
         public EmailVerifierService(EmailSender emailSender, EmailCodeGeneratorService codeGenerator, EmailVerifierRepository emailVerifier)
         {
             this.emailSender = emailSender;
             this.codeGenerator = codeGenerator;
-            this.emailVerifier = emailVerifier;
+            this.emailVerifierRepository = emailVerifier;
         }
 
         public async Task InstatiateVerifierMessage(User user)
         {
+            int timeout = await GetTimeout(user.UserId);
+            if (await GetTimeout(user.UserId) > 0)
+            {
+                throw new TimeoutException($"Email can be sent later after {timeout} seconds");
+            }
+            
             // TODO to const string;
             //send email
             int code = codeGenerator.Generate();
             string codeMsg = $"{code}";
             await emailSender.SendEmailAsync(user.Account.Email, "Подтверждение имейла InWords", codeMsg);
             //set database
-            await emailVerifier.CreateEmailVerifier(user.UserId, code);
+            await emailVerifierRepository.CreateEmailVerifier(user.UserId, code);
+        }
+
+        public async Task<int> GetTimeout(int id)
+        {
+            EmailVerifier emailVerifier = await emailVerifierRepository.FindById(id);
+            TimeSpan currentSpan = DateTime.Now - emailVerifier.SentTime - TimeSpan.FromMinutes(EMAIL_TIMEOUT);
+            int seconds = Convert.ToInt32(currentSpan.TotalSeconds);
+            return seconds;
         }
     }
 }
