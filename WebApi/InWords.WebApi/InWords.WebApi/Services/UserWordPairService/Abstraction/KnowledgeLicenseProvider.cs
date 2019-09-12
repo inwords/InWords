0c0d23ce6890c3977526f1b6c9a67a07425956f2
Alword <anzer987@yandex.ru>
2019-09-12 23:40:53 +0300

    feature Ebbinghaus curve function

--- WebApi/InWords.WebApi/InWords.WebApi/Services/UserWordPairService/Abstraction/KnowledgeLicenseProvider.cs
+++ WebApi/InWords.WebApi/InWords.WebApi/Services/UserWordPairService/Abstraction/KnowledgeLicenseProvider.cs
@@ -9,5 +9,19 @@ namespace InWords.WebApi.Services.UserWordPairService.Abstraction
     public abstract class KnowledgeLicenseProvider
     {
         public abstract KnowledgeLicense Grant(KnowledgeLicense knowledgeLicense);
+
+        /// <summary>
+        /// Implementation of 
+        /// <a href="https://en.wikipedia.org/wiki/Forgetting_curve">Forgetting curve</a>
+        /// </summary>
+        /// <param name="subconsciousLevel"></param>
+        /// <param name="MemoryLevel"></param>
+        /// <returns></returns>
+        public int Ebbinghaus(int stability, double retrievabilityLevel = 0.8)
+        {
+            double days = Math.Pow(stability, 2) + stability * Math.Log(retrievabilityLevel) + stability - Math.Log(retrievabilityLevel);
+            int daysCount = Convert.ToInt32(Math.Round(days));
+            return daysCount;
+        }
     }
 }
---
+++ WebApi/InWords.WebApi/InWords.WebApi/Services/UserWordPairService/Models/LicenseProviders/UncertainKnowledge.cs
@@ -0,0 +1,16 @@
+﻿using InWords.WebApi.Services.UserWordPairService.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InWords.WebApi.Services.UserWordPairService.Models.LicenseProviders
+{
+    public class UncertainKnowledge : KnowledgeLicenseProvider
+    {
+        public override KnowledgeLicense Grant(KnowledgeLicense knowledgeLicense)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
