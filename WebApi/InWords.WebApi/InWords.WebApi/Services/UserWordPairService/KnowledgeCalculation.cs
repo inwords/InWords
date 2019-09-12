08052446129d2982b25bd17f497ab1cc8efd904e
Alword <anzer987@yandex.ru>
2019-09-12 19:56:35 +0300

    fix misspelling

---
+++ WebApi/InWords.WebApi/InWords.WebApi/Services/UserWordPairService/KnowledgeCalculation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InWords.WebApi.Services.UserWordPairService
+{
+    public class KnowledgeCalculation
+    {
+    }
+}
---
+++ WebApi/InWords.WebApi/InWords.WebApi/Services/UserWordPairService/KnowledgeUpdate.cs
@@ -0,0 +1,29 @@
+﻿using InWords.Data.Domains;
+using InWords.Data.Repositories;
+using InWords.WebApi.Services.UserWordPairService.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InWords.WebApi.Services.UserWordPairService
+{
+    public class KnowledgeUpdate
+    {
+        private readonly UserWordPairRepository userWordPairRepository = null;
+        public KnowlengeUpdate(UserWordPairRepository userWordPairRepository)
+        {
+            this.userWordPairRepository = userWordPairRepository;
+        }
+
+        /// <summary>
+        /// This is to update <see cref="UserWordPair"/> learning period
+        /// </summary>
+        /// <param name="PairKnowledges">Int is <see cref="UserWordPair"/> id</param>
+        /// <returns></returns>
+        public async Task ByDicrinary(Dictionary<int, KnowledgeQualitys> PairKnowledges)
+        {
+
+        }
+    }
+}
