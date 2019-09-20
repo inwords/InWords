602d15180a67e9cfef32d9e8282a75923189408c
Alword <anzer987@yandex.ru>
2019-09-20 22:01:36 +0300

    feature Email lPassword Reset Service Template

---
+++ WebApi/InWords.WebApi/InWords.WebApi/Services/Email/EmailPasswordResetService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InWords.WebApi.Services.Email
+{
+    public class EmailPasswordResetService
+    {
+
+        public EmailPasswordResetService()
+        {
+
+        }
+
+        // By email, send token and guid 
+        public void SendResetPasswordMail(string email)
+        {
+
+        }
+
+        // By link guid set new password
+        public void UpdatePassword(string guid, string password)
+        {
+
+        }
+
+        // By email and key set new password
+        public void UpdatePassword(string email, int key, string password)
+        {
+
+        }
+
+    }
+}
