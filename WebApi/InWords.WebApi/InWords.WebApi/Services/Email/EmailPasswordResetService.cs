c864e3b9f4e77ab451d757762a1a8d2945663c88
Alword <anzer987@yandex.ru>
2019-09-21 18:35:26 +0300

    password reset maket

--- WebApi/InWords.WebApi/InWords.WebApi/Services/Email/EmailPasswordResetService.cs
+++ WebApi/InWords.WebApi/InWords.WebApi/Services/Email/EmailPasswordResetService.cs
@@ -8,28 +8,32 @@ namespace InWords.WebApi.Services.Email
     public class EmailPasswordResetService
     {
 
-        public EmailPasswordResetService()
+        public EmailPasswordResetService(EmailVerifierService)
         {
 
         }
 
         // By email, send token and guid 
         public void SendResetPasswordMail(string email)
-        {
-
+        { 
+            // send email
+            // write repo
         }
 
         // By link guid set new password
         public void UpdatePassword(string guid, string password)
         {
-
+            // find in repo
+            // delete if exist
+            // update password
         }
 
         // By email and key set new password
         public void UpdatePassword(string email, int key, string password)
         {
-
+            // find in repo
+            // delete if exist
+            // update password
         }
-
     }
 }
