95aebf485bd70cd9ea7adf6a5bcb33a2a7520971
Alword <anzer987@yandex.ru>
2019-10-02 20:41:04 +0300

    update api access

--- WebApi/InWords.WebApi/InWords.WebApi/Controllers/v1/CardsGame/CardGameParserController.cs
+++ WebApi/InWords.WebApi/InWords.WebApi/Controllers/v1/CardsGame/CardGameParserController.cs
@@ -1,8 +1,10 @@
 ﻿using System.Threading.Tasks;
 using InWords.Data.DTO;
 using InWords.Data.DTO.GameBox;
+using InWords.Data.Enums;
 using InWords.WebApi.Models.CardGameParser;
 using InWords.WebApi.Services.GameService;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace InWords.WebApi.Controllers.v1.CardsGame
@@ -21,6 +23,7 @@ namespace InWords.WebApi.Controllers.v1.CardsGame
 
         [HttpGet]
         [Route("UpdateGame/{gameId}")]
+        [Authorize(Roles = nameof(RoleType.Admin))]
         public async Task<IActionResult> UpdateGame(int gameId)
         {
             string source = System.IO.File.ReadAllText("AppData/CardsGames.txt");
