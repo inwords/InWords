8d79766e1776dfe7959ec37c8bf3ce7d885afd4a
Alword <anzer987@yandex.ru>
2019-10-03 22:05:41 +0300

    abstract GameCreator

---
+++ WebApi/InWords.WebApi/InWords.WebApi/Services/UserGameService/GameCreator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InWords.WebApi.Services.UserGameService
+{
+    public class GameCreator
+    {
+        public async Task CreateUserLevelAsync(int userId, IList<int> userwordpair)
+        {
+
+        }
+    }
+}
