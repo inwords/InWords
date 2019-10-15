98ec68ef91e6b6788a5d0519427555c7b89a01c7
Artyom Slepushko <anzer987@yandex.ru>
2019-10-15 13:48:29 +0300

    test replace (#99)

--- WebApi/InWords.Depricated/InWords.Service.TFA.Data/AppConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace InWords.Service.TFA.Data
-{
-    public static class AppConfig
-    {
-        public const string DATA_CONFIG = "InWords.Service.TFA.Data.Resource.TFADatabase.security.json";
-
-        //public readonly static string DefaultConnection = "DefaultConnection";
-    }
-}
