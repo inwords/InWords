7781d28d6c6540fb4fa0712c8007774d7bb26578
Alword <anzer987@yandex.ru>
2019-10-15 18:56:01 +0300

    fix dictionary collection serilization

---
+++ WebApi/InWords.WebApi/InWords.Data.DTOTests/PullWordsAnswerTests.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using InWords.Data.DTO;
+using Xunit;
+
+namespace InWords.Data.DTOTests
+{
+    public class PullWordsAnswerTests
+    {
+        [Fact]
+        public void ClassSerializeText()
+        {
+            PullWordsAnswer answer = SampleCollection();
+
+            string json = JsonSerializer.Serialize(answer);
+            var actualCAnswer = JsonSerializer.Deserialize<PullWordsAnswer>(json);
+
+            Assert.NotNull(actualCAnswer);
+            Assert.NotNull(actualCAnswer.AddedWords);
+            Assert.NotNull(actualCAnswer.RemovedServerIds);
+
+            IEnumerator<WordTranslation> enumerate = answer?.AddedWords.GetEnumerator();
+            
+            foreach (WordTranslation addedWord in actualCAnswer.AddedWords)
+            {
+                if (IsContainsEnumerator(enumerate)) continue;
+                AssertCollectionElement(enumerate, addedWord);
+                enumerate.MoveNext();
+            }
+            enumerate.Dispose();
+        }
+
+        private static void AssertCollectionElement(IEnumerator<WordTranslation> enumerate, WordTranslation addedWord)
+        {
+            Assert.Equal(enumerate.Current.WordForeign, addedWord.WordForeign);
+            Assert.Equal(enumerate.Current.WordNative, addedWord.WordNative);
+            Assert.Equal(enumerate.Current.Id, addedWord.Id);
+            Assert.Equal(enumerate.Current.ServerId, addedWord.ServerId);
+        }
+
+        private static bool IsContainsEnumerator(IEnumerator<WordTranslation> enumerate)
+        {
+            return enumerate != null && enumerate.Current == null;
+        }
+
+        private static PullWordsAnswer SampleCollection()
+        {
+            return new PullWordsAnswer
+            {
+                AddedWords = new List<WordTranslation>()
+                {
+                    new WordTranslation("test", "тест", 0), new WordTranslation("test1", "тест1", 1)
+                },
+                RemovedServerIds = new List<int>() {3, 4}
+            };
+        }
+    }
+}
