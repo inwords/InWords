718e7d7322418e6031b585a27f46615138c8a65f
Alword <anzer987@yandex.ru>
2019-11-10 14:11:38 +0300

    feature add words from game

--- WebApi/InWords.WebApi/InWords.WebApi/Services/GameWordsToDictionary/ByGameIdUserId/GameToUserHandler.cs
+++ WebApi/InWords.WebApi/InWords.WebApi/Services/GameWordsToDictionary/ByGameIdUserId/GameToUserHandler.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using InWords.Data;
+using InWords.Data.Domains;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace InWords.WebApi.Services.GameWordsToDictionary.ByGameIdUserId
 {
@@ -13,9 +16,35 @@ namespace InWords.WebApi.Services.GameWordsToDictionary.ByGameIdUserId
         {
             this.context = context;
         }
-        public Task<GameToUserQueryResult> Handle(GameToUserQuery request, CancellationToken cancellationToken)
+        public async Task<GameToUserQueryResult> Handle(GameToUserQuery request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            // find words
+            IQueryable<int> wordsId = context.WordsInGame(request.CreationId);
+
+            // exclude existing words
+            IQueryable<int> userWords = UserWords(context.UserWordPairs, request.UserId);
+            wordsId = wordsId.Except(userWords);
+
+            IQueryable<UserWordPair> userWordPairs = wordsId.Select(w =>
+                new UserWordPair()
+                {
+                    WordPairId = w,
+                    UserId = request.UserId
+                });
+
+            // add to user words
+            await context.UserWordPairs.AddRangeAsync(userWordPairs, cancellationToken).ConfigureAwait(false);
+
+            // return count
+            return new GameToUserQueryResult()
+            {
+                WordsAdded = wordsId.Count()
+            };
+        }
+
+        private IQueryable<int> UserWords(DbSet<UserWordPair> userWordPairs, int userId)
+        {
+            return userWordPairs.Where(u => u.UserId.Equals(userId)).Select(d => d.WordPairId);
         }
     }
 }
