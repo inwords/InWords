a95c7ecf9b5809c7724e9998fc776feef8869598
Alword <anzer987@yandex.ru>
2019-11-18 19:23:48 +0300

    prepare command

---
+++ WebApi/InWords.WebApi/InWords.WebApi/Services/UserGameService/GetUsersGameHistory/GetUserGameStoryHandler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InWords.WebApi.Services.UserGameService.GetUsersGameHistory
+{
+    public class GetUserGameStoryHandler
+    {
+    }
+}
