b7a145208523a626451d165a8b18e3070eb2aa19
Alword <anzer987@yandex.ru>
2019-11-21 15:27:23 +0300

    feature select game levels by game id

---
+++ WebApi/InWords.WebApi/InWords.WebApi/Services/GameService/GetGameLevels/GetLevelsByGameIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InWords.Data.DTO.GameBox;
+using MediatR;
+
+namespace InWords.WebApi.Services.GameService.GetGameLevels
+{
+    public class GetLevelsByGameIdQuery : IRequest<GameObject>
+    {
+        public int GameId { get; set; }
+    }
+}
