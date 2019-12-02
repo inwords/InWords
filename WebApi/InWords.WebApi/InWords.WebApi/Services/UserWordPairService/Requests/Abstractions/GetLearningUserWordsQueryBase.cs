22223e4e8be5b10f89a72e96d60d6e1a792bf1d4
Alword <anzer987@yandex.ru>
2019-12-02 23:28:55 +0300

    adjust namespaces

--- WebApi/InWords.WebApi/InWords.WebApi/Services/UserWordPairService/Requests/Abstractions/GetLearningUserWordsQueryBase.cs
+++ WebApi/InWords.WebApi/InWords.WebApi/Services/UserWordPairService/Requests/Abstractions/GetLearningUserWordsQueryBase.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace InWords.WebApi.Services.UserWordPairService.Requests
+﻿namespace InWords.WebApi.Services.UserWordPairService.Requests.Abstractions
 {
     public class GetLearningUserWordsQueryBase
     {
--- WebApi/InWords.WebApi/InWords.WebApi/Services/UserWordPairService/Requests/GetLearningWordsIds/GetLearningUserWordsIdsQuery.cs
+++ WebApi/InWords.WebApi/InWords.WebApi/Services/UserWordPairService/Requests/GetLearningWordsIds/GetLearningUserWordsIdsQuery.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using InWords.Data.DTO;
+using InWords.WebApi.Services.UserWordPairService.Requests.Abstractions;
 using MediatR;
 
 namespace InWords.WebApi.Services.UserWordPairService.Requests.GetLearningWordsIds
