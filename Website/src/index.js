69b2a73146ab414d116142261a2df43149b43fa6
Prometium <akrasnovskii@yandex.ru>
2020-01-12 19:36:12 +0300

    improve base styles

--- Website/src/index.js
+++ Website/src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { register } from './serviceWorker';
 import { Provider } from 'react-redux';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import store from './store';
 import App from './App';
 
-import './theme/theme.scss';
-import './fonts/index.css';
+import 'normalize.css';
+
+import 'src/fonts/index.css';
+import 'src/theme/theme.scss';
 
 ReactDOM.render(
   <Provider store={store}>
-    <CssBaseline />
     <App />
   </Provider>,
   document.getElementById('root')
