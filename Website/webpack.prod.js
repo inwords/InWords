e7b856f4ea1b97bad51d2cd98c5f5ea7c97c57e6
Prometium <akrasnovskii@yandex.ru>
2020-01-13 15:50:59 +0300

    configure postcss

--- Website/webpack.prod.js
+++ Website/webpack.prod.js
@@ -29,11 +29,16 @@ module.exports = merge(common, {
     rules: [
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
       },
       {
         test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+        ]
       },
       {
         test: /\.(woff|woff2)$/,
