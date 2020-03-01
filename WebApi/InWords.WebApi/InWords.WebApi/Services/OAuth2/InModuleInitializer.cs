b26a052b605849c23e40555562b97b36dd4edaf4
Alword <anzer987@yandex.ru>
2020-03-01 13:18:37 +0300

    install google auth package

---
+++ WebApi/InWords.WebApi/InWords.WebApi/Services/OAuth2/InModuleInitializer.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using InWords.Service.Encryption;
+using InWords.WebApi.Module;
+using InWords.WebApi.Services.Users.EmailUpdate;
+using InWords.WebApi.Services.Users.Registration;
+using InWords.WebApi.Services.Users.Token;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InWords.WebApi.Services.OAuth2
+{
+    public class InModuleInitializer : InModule
+    {
+        public override void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthorization();
+            services.AddGoogle()
+        }
+    }
+}
