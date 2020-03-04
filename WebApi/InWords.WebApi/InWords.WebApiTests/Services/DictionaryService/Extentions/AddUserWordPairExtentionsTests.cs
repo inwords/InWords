9e1b567c6631fef9259941e834fb7e30a0490089
Alword <anzer987@yandex.ru>
2020-03-04 23:10:14 +0300

    implement test

---
+++ WebApi/InWords.WebApi/InWords.WebApiTests/Services/DictionaryService/Extentions/AddUserWordPairExtentionsTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace InWords.WebApiTests.Services.DictionaryService.Extentions
+{
+    public class AddUserWordPairExtentionsTests
+    {
+        [Fact]
+        public void AddUserWordsPairsTest() 
+        {
+            // todo continue here
+        }
+    }
+}
