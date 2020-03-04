211391833739ae38d6a75168e35cccd62871d93d
Alword <anzer987@yandex.ru>
2020-03-04 23:00:55 +0300

    feature AddUserWordPair

--- WebApi/InWords.WebApi/InWords.WebApi/Services/DictionaryService/Extentions/AddUserWordPairExtentions.cs
+++ WebApi/InWords.WebApi/InWords.WebApi/Services/DictionaryService/Extentions/AddUserWordPairExtentions.cs
@@ -1,28 +1,45 @@
-﻿using InWords.Data.Domains;
+﻿using InWords.Common.Extensions;
+using InWords.Data.Domains;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace InWords.WebApi.Services.DictionaryService.Extentions
 {
     public static class AddUserWordPairExtentions
     {
-        public static void AddUserWordPair(this DbSet<UserWordPair> userWordPairs, IEnumerable<UserWordPair> content)
+        public static void AddUserWordPair(this DbSet<UserWordPair> userWordPairs, IEnumerable<UserWordPair> request)
         {
-            var ids = content.Select(d => d.WordPairId).ToArray();
-            var addedWords = userWordPairs.Where(u => ids.Any(i => i == u.WordPairId)).ToArray();
+            if (userWordPairs == null)
+                throw new ArgumentNullException($"{nameof(userWordPairs)}");
 
-            //var pairsToAdd = UserWordPair
-            //foreach (var pair in content)
-            //{
-            //    var addedPair = addedWords.SingleOrDefault(added => added.WordPairId == pair.WordPairId && added.IsInvertPair == pair.IsInvertPair);
-            //    if (addedPair == null) 
-            //    {
+            request = request.ToList();
+            int userId = request.First().UserId;
+            var groups = request.GroupBy(d => d.UserId).ToDictionary(d => d.Key, s => s.ToList());
+            if (groups.Keys.Count > 1)
+            {
+                throw new NotImplementedException();
+            }
 
-            //    }
-            //}
+            var ids = request.Select(d => d.WordPairId).ToArray();
+            var existedWords = userWordPairs.Where(u => u.UserId == userId && ids.Any(i => i == u.WordPairId))
+                .ToDictionary(d => (d.WordPairId, d.IsInvertPair));
+
+            var pairsToAdd = new List<UserWordPair>(request.Count());
+            foreach (var requestWord in request)
+            {
+                var key = (requestWord.WordPairId, requestWord.IsInvertPair);
+                if (existedWords.ContainsKey(key))
+                {
+                    requestWord.UserWordPairId = existedWords[key].UserWordPairId;
+                }
+                else
+                {
+                    pairsToAdd.Add(requestWord);
+                }
+            }
+            userWordPairs.AddRange(pairsToAdd);
         }
     }
 }
