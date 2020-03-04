6ef93ce16a0e593bd5a77b9b86c5217576ea0059
Alword <anzer987@yandex.ru>
2020-03-04 16:29:27 +0300

    feature long request logging

--- WebApi/InWords.WebApi/InWords.WebApi/Services/Abstractions/ContextRequestHandler.cs
+++ WebApi/InWords.WebApi/InWords.WebApi/Services/Abstractions/ContextRequestHandler.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -12,6 +13,8 @@ namespace InWords.WebApi.Services.Abstractions
         [Inject]
         private ILogger logger { get; set; }
         private readonly TContext context;
+        private Stopwatch stopwatch;
+
         public ContextRequestHandler(TContext context)
         {
             this.context = context;
@@ -19,18 +22,19 @@ namespace InWords.WebApi.Services.Abstractions
 
         protected TContext Context => context;
 
-        public Task<TResult> Handle(TQuery request, CancellationToken cancellationToken = default)
+        public async Task<TResult> Handle(TQuery request, CancellationToken cancellationToken = default)
         {
             // TODO: Logging
-            try
+
+            stopwatch.Start();
+            var result = await HandleRequest(request, cancellationToken).ConfigureAwait(false);
+            stopwatch.Stop();
+            if (stopwatch.ElapsedMilliseconds > 500)
             {
-                return HandleRequest(request, cancellationToken);
+                var name = typeof(TQuery).Name;
+                logger.LogWarning($"InWords Long Running Request: {name} ({stopwatch.ElapsedMilliseconds} milliseconds) {request}");
             }
-            catch (Exception e)
-            {
-                logger.LogError(e, $"{e.Message} on request {request.ToString()}");
-                throw;
-            }
+            return result;
         }
 
         public virtual Task<TResult> HandleRequest(TQuery request, CancellationToken cancellationToken = default)
