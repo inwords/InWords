6eaaa5ebfb4292543ce5bf0407e7fc96a4902fc3
Alword <anzer987@yandex.ru>
2020-03-06 21:19:01 +0300

    feature addwords controller

--- WebApi/InWords.WebApi/InWords.WebApi/Services/DictionaryService/Words/AddWords.cs
+++ WebApi/InWords.WebApi/InWords.WebApi/Services/DictionaryService/Words/AddWords.cs
@@ -25,40 +25,59 @@ namespace InWords.WebApi.Services.DictionaryService.Words
             if (request == null)
                 throw new ArgumentNullException($"{nameof(request)}");
 
-            var userId = request.UserId;
+            int userId = request.UserId;
             var requestData = request.Value;
 
             // Add words transation (savechanges)
             List<Word> dataBaseWords = await AddWordsAsync(requestData).ConfigureAwait(false);
 
             // Add wordPairs transaction 
-            List<WordPair> wordPairsToAdd = SelectWordPairs(requestData, dataBaseWords);
-            var map = Context.WordPairs.AddWordPairs(wordPairsToAdd);
+            List<(int, WordPair)> pairsToAdd = SelectWordPairs(requestData, dataBaseWords);
+            HashSet<int> inverted = Context.WordPairs.AddWordPairs(pairsToAdd.Select(d => d.Item2));
             await Context.SaveChangesAsync().ConfigureAwait(false);
 
             // Add user's word pair transation
-            List<UserWordPair> userWords = wordPairsToAdd.Select(pair => new UserWordPair
-            {
-                UserId = userId,
-                WordPairId = pair.WordPairId,
-                IsInvertPair = map.Contains(pair.WordPairId)
-            }).ToList();
+            (int, UserWordPair)[] userWords = ConvertToUserWordPairs(userId, pairsToAdd, inverted).ToArray();
+            Context.UserWordPairs.AddUserWordPair(userWords.Select(d => d.Item2));
+            await Context.SaveChangesAsync().ConfigureAwait(false);
 
-            // return result;
-            throw new NotImplementedException();
+
+            // form reply
+            AddWordsReply addWordsReply = new AddWordsReply();
+            foreach (var word in userWords)
+            {
+                AddWordReply addWordReply = new AddWordReply
+                {
+                    LocalId = word.Item1,
+                    ServerId = word.Item2.UserWordPairId
+                };
+                addWordsReply.WordIds.Add(addWordReply);
+            }
+
+            return addWordsReply;
         }
 
-        private static List<WordPair> SelectWordPairs(AddWordsRequest requestData, List<Word> dataBaseWords)
+        private static IEnumerable<(int, UserWordPair)> ConvertToUserWordPairs(int userId, IEnumerable<(int, WordPair)> wordPairsToAdd, HashSet<int> inverted)
+        {
+            return wordPairsToAdd.Select(pair => (pair.Item1, new UserWordPair
+            {
+                UserId = userId,
+                WordPairId = pair.Item2.WordPairId,
+                IsInvertPair = inverted.Contains(pair.Item2.WordPairId)
+            }));
+        }
+
+        private static List<(int, WordPair)> SelectWordPairs(AddWordsRequest requestData, List<Word> dataBaseWords)
         {
             var dictionary = dataBaseWords.ToContentDictionary();
-            var wordPairsToAdd = new List<WordPair>();
+            var wordPairsToAdd = new List<(int, WordPair)>();
             requestData.Words.ForEach(wp =>
             {
-                wordPairsToAdd.Add(new WordPair()
+                wordPairsToAdd.Add((wp.LocalId, new WordPair()
                 {
                     WordForeignId = dictionary[wp.WordForeign.ToNormalizedWord()].WordId,
                     WordNativeId = dictionary[wp.WordNative.ToNormalizedWord()].WordId
-                });
+                }));
             });
             return wordPairsToAdd;
         }
---
+++ WebApi/InWords.WebApi/InWords.WebApi/gRPC/Services/DictionaryService.cs
@@ -0,0 +1,34 @@
+﻿using Grpc.Core;
+using InWords.Service.Auth.Extensions;
+using InWords.WebApi.Services.Abstractions;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Threading.Tasks;
+
+namespace InWords.WebApi.gRPC.Services
+{
+    [Authorize]
+    public class DictionaryService : DictionaryProvider.DictionaryProviderBase
+    {
+        IMediator mediator;
+        public DictionaryService(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+        public override async Task<AddWordsReply> AddWords(AddWordsRequest request, ServerCallContext context)
+        {
+            var reqestObject = new AuthorizedRequestObject<AddWordsRequest, AddWordsReply>(request)
+            {
+                UserId = context.GetHttpContext().User.GetUserId()
+            };
+            AddWordsReply reply = await mediator.Send(reqestObject).ConfigureAwait(false);
+            return reply;
+        }
+
+        public override Task<WordsReply> GetWords(GetWordsRequest request, ServerCallContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
