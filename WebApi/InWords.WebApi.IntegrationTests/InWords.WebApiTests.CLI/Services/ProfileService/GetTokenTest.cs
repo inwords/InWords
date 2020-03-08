1f6d2f1c8f00f8bfe777f7df217e131d36b16d09
Alword <anzer987@yandex.ru>
2020-03-08 17:04:27 +0300

    replase integrations tests

--- WebApi/InWords.WebApi.IntegrationTests/InWords.WebApiTests.CLI/Services/ProfileService/GetTokenTest.cs
+++ WebApi/InWords.WebApi.IntegrationTests/InWords.WebApiTests.CLI/Services/ProfileService/GetTokenTest.cs
@@ -2,10 +2,6 @@
 using Grpc.Net.Client;
 using InWords.WebApiTest.gRPC.Services;
 using InWords.WebApiTests.CLI.TestUtils;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 using static InWords.WebApiTest.gRPC.Services.Profile;
 
