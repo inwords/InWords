c19e56eae118f0340da4d769a868ad3010f98cd4
Alword <anzer987@yandex.ru>
2020-03-15 20:18:59 +0300

    folder rename

---
+++ WebApi/InWords.WebApi/InWords.WebApi/Services/OAuth2/Requests/GoogleAuth.cs
@@ -0,0 +1,25 @@
+﻿using InWords.Data;
+using InWords.WebApi.gRPC.Services;
+using InWords.WebApi.Services.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace InWords.WebApi.Services.OAuth2.Requests
+{
+    public class GoogleAuth : StructRequestHandler<OAuthTokenRequest, TokenReply, InWordsDataContext>
+    {
+        public GoogleAuth(InWordsDataContext context) : base(context)
+        {
+        }
+
+        public override Task<TokenReply> HandleRequest(RequestObject<OAuthTokenRequest, TokenReply> request, 
+            CancellationToken cancellationToken = default)
+        {
+
+            return base.HandleRequest(request, cancellationToken);
+        }
+    }
+}
