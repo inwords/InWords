4cd800125d043f217c9eb48ceb5e5174b68aba33
Alword <anzer987@yandex.ru>
2020-03-17 17:56:37 +0300

    feature v2 methods

---
+++ WebApi/InWords.WebApi/InWords.WebApi/Services/DictionaryService/Words/DeleteWords.cs
@@ -0,0 +1,12 @@
+﻿using InWords.WebApi.Services.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InWords.WebApi.Services.DictionaryService.Words
+{
+    public class DeleteWords : AuthorizedRequestObjectHandler<AddWordsRequest, AddWordsReply, InWordsDataContext>
+    {
+    }
+}
