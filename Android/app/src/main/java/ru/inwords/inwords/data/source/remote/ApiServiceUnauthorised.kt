5e9a4cef50e817eee428d3d16bda7dbeb779a3f8
rugged-bl <vasia.shumilov@gmail.com>
2020-03-17 12:33:54 +0300

    new proto files used

--- Android/app/src/main/java/ru/inwords/inwords/data/source/remote/ApiServiceUnauthorised.kt
+++ Android/app/src/main/java/ru/inwords/inwords/data/source/remote/ApiServiceUnauthorised.kt
@@ -1,27 +1,15 @@
 package ru.inwords.inwords.data.source.remote
 
 import io.reactivex.Single
-import retrofit2.http.*
+import retrofit2.http.Body
+import retrofit2.http.Header
+import retrofit2.http.Headers
+import retrofit2.http.POST
 import ru.inwords.inwords.BuildConfig
-import ru.inwords.inwords.data.source.remote.session.TokenResponse
-import ru.inwords.inwords.profile.data.bean.UserCredentials
 import ru.inwords.inwords.texttospeech.data.bean.TtsSynthesizeRequest
 import ru.inwords.inwords.texttospeech.data.bean.TtsSynthesizeResponse
 
 interface ApiServiceUnauthorised {
-    @GET("/v1.0/values")
-    fun getValues(): Single<List<String>>
-
-    //USERS
-    @POST("/v1.0/auth/registration")
-    @Headers("Content-Type: application/json", "x-api-version: 2.0")
-    fun registerUser(@Body userCredentials: UserCredentials): Single<TokenResponse>
-
-    @POST("v1.0/auth/token")
-    //TODO think about it
-    @Headers("x-api-version: 2.0")
-    fun getToken(@Body credentials: UserCredentials): Single<TokenResponse>
-
     //GOOGLE
     @POST(BuildConfig.TTS_GOOGLE_API_URL + "/v1beta1/text:synthesize")
     @Headers("content-type: application/json; charset=UTF-8")
