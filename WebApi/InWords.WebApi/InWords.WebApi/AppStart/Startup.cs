9996a5c550b76b4898a9516fb4c4154912b6e478
Alword <anzer987@yandex.ru>
2020-03-23 19:09:47 +0300

    stay at proto's bug need to fix

--- WebApi/InWords.WebApi/InWords.WebApi/AppStart/Startup.cs
+++ WebApi/InWords.WebApi/InWords.WebApi/AppStart/Startup.cs
@@ -88,7 +88,7 @@ namespace InWords.WebApi.AppStart
 
             // Register the Swagger generator, defining 1 or more Swagger documents
             services.AddSwaggerInWords();
-
+            services.AddSingleton<IConfiguration>(Configuration);
             services.AddHttpClient();
             // to register types of modules
             Program.InModules.ForEach(m => m.ConfigureServices(services));
