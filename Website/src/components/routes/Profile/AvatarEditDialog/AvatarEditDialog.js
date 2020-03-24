a2ad1148a6de26c9e72ed6e809d652d3465fd553
Prometium <akrasnovskii@yandex.ru>
2020-03-25 01:52:34 +0300

    fix dialog styles

--- Website/src/components/routes/Profile/AvatarEditDialog/AvatarEditDialog.js
+++ Website/src/components/routes/Profile/AvatarEditDialog/AvatarEditDialog.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Dialog from 'src/components/core/Dialog';
 import DialogTitle from 'src/components/core/DialogTitle';
--- Website/src/components/routes/common/ValuableSlider/ValuableSlider.js
+++ Website/src/components/routes/common/ValuableSlider/ValuableSlider.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes, { string } from 'prop-types';
+import PropTypes from 'prop-types';
 import Typography from 'src/components/core/Typography';
 import Slider from 'src/components/core/Slider';
 
