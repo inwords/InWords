232858489c3ccf61124840b305b178d11c736db9
rugged-bl <vasia.shumilov@gmail.com>
2020-03-26 11:04:06 +0300

    ManagedChannelProvider moved to core

--- Android/app/src/main/java/ru/inwords/inwords/core/grpc/ManagedChannelProvider.kt
+++ Android/app/src/main/java/ru/inwords/inwords/core/grpc/ManagedChannelProvider.kt
@@ -1,17 +1,16 @@
-package ru.inwords.inwords.data.source.remote
+package ru.inwords.inwords.core.grpc
 
 import android.content.Context
 import io.grpc.ManagedChannel
 import io.grpc.cronet.CronetChannelBuilder
 import org.chromium.net.ExperimentalCronetEngine
 import ru.inwords.inwords.BuildConfig
-import ru.inwords.inwords.core.grpc.LoggingClientInterceptor
 
-fun buildManagedChannel(context: Context): ManagedChannel {
+fun buildManagedChannel(context: Context, grpcApiUrl: String): ManagedChannel {
     val engine = ExperimentalCronetEngine.Builder(context)
         .enableQuic(true)
         .build()
-    return CronetChannelBuilder.forAddress(BuildConfig.GRPC_API_URL, 443, engine)
+    return CronetChannelBuilder.forAddress(grpcApiUrl, 443, engine)
         .apply {
             if (BuildConfig.DEBUG) {
                 intercept(LoggingClientInterceptor())
