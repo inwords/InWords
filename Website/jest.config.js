eb99cf7d8dcf1a5fb2a9a35486179f8e422d9979
Prometium <akrasnovskii@yandex.ru>
2020-04-27 21:34:48 +0300

    fix test mock

--- Website/jest.config.js
+++ Website/jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   moduleNameMapper: {
     'src/(.*)': '<rootDir>/src/$1',
-    '\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.js'
+    '\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.js',
+    '\\.png$': '<rootDir>/__mocks__/fileMock.js'
   },
   moduleFileExtensions: ['js']
 };
