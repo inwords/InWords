25884ce1c8a235c17f378c08e740a7581a419b69
Alword <anzer987@yandex.ru>
2020-04-27 14:33:47 +0300

    add card game controller

---
+++ WebApi/InWords.WebApi/InWords.WebApi/gRPC/Services/ClassicCardGameService.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Core;
+using InWords.Protobuf;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InWords.WebApi.gRPC.Services
+{
+    public class ClassicCardGameService : ClassicCardGameProvider.ClassicCardGameProviderBase
+    {
+        private IMediator mediator;
+        public ClassicCardGameService(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        public override Task<LevelPoints> Estimate(CardGameMetrics request, ServerCallContext context)
+        {
+            return base.Estimate(request, context);
+        }
+    }
+}
