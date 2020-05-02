918000ab3f0895d303c7d69599972c7ca3f54bee
Alword <anzer987@yandex.ru>
2020-05-02 14:02:19 +0300

    feature training words

--- WebApi/InWords.WebApi/InWords.WebApi/Modules/DictionaryServiceHandler/Words/GetTrainingWords.cs
+++ WebApi/InWords.WebApi/InWords.WebApi/Modules/DictionaryServiceHandler/Words/GetTrainingWords.cs
@@ -1,22 +1,43 @@
 ﻿using InWords.Data;
 using InWords.Protobuf;
+using InWords.WebApi.Modules.DictionaryServiceHandler.Extentions;
 using InWords.WebApi.Services.Abstractions;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using static InWords.Protobuf.TrainingReply.Types;
 
 namespace InWords.WebApi.Modules.DictionaryServiceHandler.Words
 {
-    public class GetTrainingWords : AuthorizedRequestObjectHandler<DeleteWordsRequest, Empty, InWordsDataContext>
+    public class GetTrainingWords : AuthorizedRequestObjectHandler<Empty, TrainingReply, InWordsDataContext>
     {
         public GetTrainingWords(InWordsDataContext context) : base(context) { }
 
-        public override Task<Empty> HandleRequest(AuthorizedRequestObject<DeleteWordsRequest, Empty> request, 
+        public override async Task<TrainingReply> HandleRequest(
+            AuthorizedRequestObject<Empty, TrainingReply> request, 
             CancellationToken cancellationToken = default)
         {
-            return base.HandleRequest(request, cancellationToken);
+            if (request == null)
+                throw new ArgumentNullException(nameof(request));
+
+            var userId = request.UserId;
+            var value = request.Value;
+            var inRangeWords = await Context.UserWordPairs.TrainingWords(userId)
+                .Select(d => new TrainingPair() 
+                {
+                    ForeignWord = d.ForeignWord,
+                    NativeWord = d.NativeWord,
+                    UserWordPair = d.UserWordPairId
+                })
+                .ToArrayAsync()
+                .ConfigureAwait(false);
+
+            TrainingReply trainingIdsReply = new TrainingReply();
+            trainingIdsReply.Pairs.AddRange(inRangeWords);
+            return trainingIdsReply;
         }
     }
 }
