7210b7bbc38c2c0de02232565dd520a6f00b8ec1
Alword <anzer987@yandex.ru>
2020-05-03 18:09:47 +0300

    feature AddOrGetUserHistoryGame

---
+++ WebApi/InWords.WebApi/InWords.WebApi/Modules/ClassicCardGame/Extentions/InWordsContextExtention.cs
@@ -0,0 +1,68 @@
+﻿using InWords.Common.Extensions;
+using InWords.Data;
+using InWords.Data.Creations;
+using InWords.Data.DTO.GameBox;
+using InWords.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InWords.WebApi.Modules.ClassicCardGame.Extentions
+{
+    public static class InWordsContextExtention
+    {
+        public static async Task<int> AddOrGetUserHistoryGame(this InWordsDataContext context, int userId)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+
+            var historyGame = (from game in context.Games.Where(d => d.CreatorId == userId)
+                               join tag in context.GameTags.Where(d => d.Tags == GameTags.CustomLevelsHistory)
+                               on game.GameId equals tag.GameId
+                               select game).OrderBy(g => g.GameId).ToArray();
+
+            int historyGameId = 0;
+            if (historyGame.Any())
+            {
+                historyGameId = historyGame[0].GameId;
+
+                if (historyGame.Length > 1)
+                {
+                    var historyCollisions = historyGame.Skip(1);
+
+                    int[] ids = historyCollisions.Select(d => d.GameId).ToArray();
+                    var levels = context.GameLevels.Where(g => ids.Contains(g.GameId));
+
+                    levels.ForEach(level =>
+                    {
+                        level.GameId = historyGameId;
+                    });
+                    await context.SaveChangesAsync().ConfigureAwait(false);
+
+                    context.Remove(historyCollisions);
+                    await context.SaveChangesAsync().ConfigureAwait(false);
+                }
+            }
+            else
+            {
+                Game game = new Game
+                {
+                    CreatorId = userId
+                };
+                context.Add(game);
+                await context.SaveChangesAsync().ConfigureAwait(false);
+                GameTag historyGameTag = new GameTag()
+                {
+                    GameId = game.GameId,
+                    Tags = GameTags.CustomLevelsHistory,
+                    UserId = userId
+                };
+                await context.SaveChangesAsync().ConfigureAwait(false);
+
+                historyGameId = game.GameId;
+            }
+            return historyGameId;
+        }
+    }
+}
---
+++ WebApi/InWords.WebApi/InWords.WebApi/Modules/ClassicCardGame/Service/SaveLevels.cs
@@ -0,0 +1,43 @@
+﻿using InWords.Common.Extensions;
+using InWords.Data;
+using InWords.Data.Creations;
+using InWords.Data.Creations.GameBox;
+using InWords.Data.Enums;
+using InWords.Protobuf;
+using InWords.WebApi.Modules.ClassicCardGame.Extentions;
+using InWords.WebApi.Services.Abstractions;
+using Org.BouncyCastle.Ocsp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace InWords.WebApi.Modules.ClassicCardGame.Service
+{
+    public class SaveLevels : AuthorizedRequestObjectHandler<CardGameInfos, LevelPoints, InWordsDataContext>
+    {
+        public SaveLevels(InWordsDataContext context) : base(context) { }
+
+        public override async Task<LevelPoints> HandleRequest(
+            AuthorizedRequestObject<CardGameInfos, LevelPoints> request,
+            CancellationToken cancellationToken = default)
+        {
+
+            if (request == null)
+                throw new ArgumentNullException(nameof(request));
+
+            var userId = request.UserId;
+            var value = request.Value;
+
+            var historyGameId = await Context.AddOrGetUserHistoryGame(userId).ConfigureAwait(false);
+            
+            // add level
+            value.Info.Select(new GameLevel)
+
+            // calculate metrics
+
+            throw new NotImplementedException();
+        }
+    }
+}
