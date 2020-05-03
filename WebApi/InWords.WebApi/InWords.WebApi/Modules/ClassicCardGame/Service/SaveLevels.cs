c55eb464ce7352dfe1df6d31ee66d4d7948f28a4
Alword <anzer987@yandex.ru>
2020-05-03 18:43:00 +0300

    add create levles method

--- WebApi/InWords.WebApi/InWords.WebApi/Modules/ClassicCardGame/Service/SaveLevels.cs
+++ WebApi/InWords.WebApi/InWords.WebApi/Modules/ClassicCardGame/Service/SaveLevels.cs
@@ -2,10 +2,15 @@
 using InWords.Data;
 using InWords.Data.Creations;
 using InWords.Data.Creations.GameBox;
+using InWords.Data.Domains;
+using InWords.Data.DTO.GameBox;
 using InWords.Data.Enums;
 using InWords.Protobuf;
+using InWords.WebApi.Extensions.InWordsDataContextExtention;
 using InWords.WebApi.Modules.ClassicCardGame.Extentions;
 using InWords.WebApi.Services.Abstractions;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using MimeKit.Encodings;
 using Org.BouncyCastle.Ocsp;
 using System;
 using System.Collections.Generic;
@@ -28,16 +33,53 @@ namespace InWords.WebApi.Modules.ClassicCardGame.Service
                 throw new ArgumentNullException(nameof(request));
 
             var userId = request.UserId;
-            var value = request.Value;
+            var info = request.Value.Info;
 
             var historyGameId = await Context.AddOrGetUserHistoryGame(userId).ConfigureAwait(false);
             
             // add level
-            value.Info.Select(new GameLevel)
-
+            var list = info.Select(d => d.WordIdOpenCount.Select(d => d.Key).ToArray()).ToList();
+            var levels = CreateLevels(historyGameId, userId, list);
+            
             // calculate metrics
 
+
             throw new NotImplementedException();
+        }
+
+        private async Task<int[]> CreateLevels(int gameId, int userId, IList<int[]> pairsInLevels)
+        {
+            var levels = pairsInLevels.Select(d =>
+            {
+                var game = new GameLevel()
+                {
+                    GameId = gameId
+                };
+                Context.Add(game);
+                return game;
+            }).ToArray();
+            await Context.SaveChangesAsync().ConfigureAwait(false);
+
+            for (int i = 0; i < pairsInLevels.Count; i++)
+            {
+                var currentLevel = levels[i];
+                int[] currentWordsIds = pairsInLevels[i].ToArray();
+
+                var currentWords = Context.CurrentUserWordPairs(userId)
+                    .Where(u => currentWordsIds.Contains(u.UserWordPairId))
+                    .ToArray();
+
+                var gameLevelWords = currentWords.Select(w => new GameLevelWord()
+                {
+                    ForeignWord = w.ForeignWord,
+                    NativeWord = w.NativeWord,
+                    GameLevelId = currentLevel.GameLevelId,
+                }).ToArray();
+
+                Context.GameLevelWords.AddRange(gameLevelWords);
+            }
+            await Context.SaveChangesAsync().ConfigureAwait(false);
+            return levels.Select(level => level.GameLevelId).ToArray();
         }
     }
 }
