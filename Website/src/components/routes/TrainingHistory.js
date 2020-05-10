6cc8552611b284ecf2e1fddae8c1cd246499786e
Prometium <akrasnovskii@yandex.ru>
2020-05-10 18:08:51 +0300

    return 'gameType'

--- Website/src/components/routes/TrainingHistory.js
+++ Website/src/components/routes/TrainingHistory.js
@@ -13,6 +13,10 @@ import CardActions from 'src/components/core/CardActions';
 import Typography from 'src/components/core/Typography';
 import LinkButton from 'src/components/core/LinkButton';
 
+const traininTypesMap = {
+  [0]: 'Закрытые карточки'
+};
+
 function TrainingHistory() {
   const trainingHistory = useSelector(store => store.trainingHistory);
 
@@ -33,12 +37,12 @@ function TrainingHistory() {
 
   return (
     <Grid spacing={3}>
-      {trainingHistory.map(({ levelId, stars }) => (
+      {trainingHistory.map(({ levelId, stars, gameType }) => (
         <GridItem key={levelId} xs={12} sm={6} md={4} lg={3}>
           <Card>
             <CardContent>
               <Typography component="h2" variant="h6">
-                Закрытые карточки
+                {traininTypesMap[gameType] || 'Неизвестная тренировка'}
               </Typography>
               <Typography component="p" color="text-secondary" gutterBottom>
                 #{levelId}
@@ -51,9 +55,9 @@ function TrainingHistory() {
             </CardContent>
             <CardActions>
               <LinkButton
-                data-testid={`to-training-${levelId}-0`}
+                data-testid={`to-training-${levelId}-${gameType}`}
                 component={RouterLink}
-                to={`${match.url}/${levelId}/0`}
+                to={`${match.url}/${levelId}/${gameType}`}
                 variant="text"
                 color="primary"
               >
