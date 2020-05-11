b02a4824df7ad693c7a53cf89959a2c258b0145b
Alword <anzer987@yandex.ru>
2020-05-11 18:08:52 +0300

    feature get current account test

---
+++ WebApi/InWords.WebApi/InWords.WebApiTests/Moduls/Profile/PublicData/GetCurrentProfileTests.cs
@@ -0,0 +1,36 @@
+﻿using InWords.Data;
+using InWords.Protobuf;
+using InWords.WebApi.Modules.Profile.PublicData;
+using InWords.WebApi.Services.Abstractions;
+using InWords.WebApiTests.TestUtils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace InWords.WebApiTests.Moduls.Profile.PublicData
+{
+    public class GetCurrentProfileTests
+    {
+        [Fact]
+        public async void Find_Existed_Account()
+        {
+            // arrange
+            int userId = 1;
+            await using InWordsDataContext context = InWordsDataContextFactory.Create();
+            await context.AddAccount(userId);
+            await context.SaveChangesAsync();
+
+            var request = new AuthorizedRequestObject<Empty, ProfileReply>(new Empty())
+            {
+                UserId = userId,
+            };
+
+            var handler = new GetCurrentProfile(context);
+            var result = await handler.Handle(request);
+
+            // assert
+            Assert.Equal(userId, result.UserId);
+        }
+    }
+}
