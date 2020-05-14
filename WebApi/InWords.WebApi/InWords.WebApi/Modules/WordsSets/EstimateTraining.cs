9f9594a008751d84ad73a744600f622b5ea85b0d
Alword <anzer987@yandex.ru>
2020-05-14 20:18:29 +0300

    feature score method

--- WebApi/InWords.WebApi/InWords.WebApi/Modules/WordsSets/EstimateTraining.cs
+++ WebApi/InWords.WebApi/InWords.WebApi/Modules/WordsSets/EstimateTraining.cs
@@ -1,6 +1,7 @@
 ﻿using Google.Protobuf.Collections;
 using InWords.Data;
 using InWords.Protobuf;
+using InWords.WebApi.Business.GameEvaluator.Game;
 using InWords.WebApi.Extensions.InWordsDataContextExtentions;
 using InWords.WebApi.Model.UserWordPair;
 using InWords.WebApi.Modules.ClassicCardGame.Extentions;
@@ -30,13 +31,25 @@ namespace InWords.WebApi.Modules.WordsSets
             int userId = request.UserId;
             var metrics = request.Value.Metrics;
 
-            // select levels to create
+            // select levels to create neet to union inside
+            // todo invent method that save cust training from IGamelevel ids
             metrics = await metrics.SaveCustomTraining(Context, userId).ConfigureAwait(false);
 
-            // calculate score
+            // configure game level managers
+            var cardGameManager = metrics
+                .Select(d => new CardGameLevel(d.GameLevelId, d.CardsWordIdOpenCount) as IGameLevel)
+                .Union(metrics.Select(d => new AudioGameLevel(d.GameLevelId, d.AuditionWordIdOpenCount)));
+            // TO-DO just union more levels types
+
+            var scoreTask = cardGameManager.Select(d => d.Score());
+            var wordsMemoryTask = cardGameManager.Select(d => d.Qualify());
+            
+            // TODO save history games
+            Context.AddOrUpdateUserGameLevel(scoreTask, userId);
+
 
             // calculate words metrics
-
+            await Context.SaveChangesAsync().ConfigureAwait(false);
             return await base.HandleRequest(request, cancellationToken).ConfigureAwait(false);
         }
 
