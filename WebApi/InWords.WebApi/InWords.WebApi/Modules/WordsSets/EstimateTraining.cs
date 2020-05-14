18b96a94f383d5e8d3ff16570cd0d71a979fb257
Alword <anzer987@yandex.ru>
2020-05-14 20:05:48 +0300

    fix metrics save

--- WebApi/InWords.WebApi/InWords.WebApi/Modules/WordsSets/EstimateTraining.cs
+++ WebApi/InWords.WebApi/InWords.WebApi/Modules/WordsSets/EstimateTraining.cs
@@ -1,8 +1,10 @@
-﻿using InWords.Data;
+﻿using Google.Protobuf.Collections;
+using InWords.Data;
 using InWords.Protobuf;
 using InWords.WebApi.Extensions.InWordsDataContextExtentions;
 using InWords.WebApi.Model.UserWordPair;
 using InWords.WebApi.Modules.ClassicCardGame.Extentions;
+using InWords.WebApi.Modules.WordsSets.Extentions;
 using InWords.WebApi.Modules.WordsSets.Models;
 using InWords.WebApi.Services.Abstractions;
 using System;
@@ -16,8 +18,6 @@ namespace InWords.WebApi.Modules.WordsSets
     public class EstimateTraining
         : AuthorizedRequestObjectHandler<TrainingDataRequest, TrainingScoreReply, InWordsDataContext>
     {
-        private IEnumerable<object> knowledgeQualifiers;
-
         public EstimateTraining(InWordsDataContext context) : base(context) { }
 
         public override async Task<TrainingScoreReply> HandleRequest(
@@ -31,50 +31,14 @@ namespace InWords.WebApi.Modules.WordsSets
             var metrics = request.Value.Metrics;
 
             // select levels to create
-            var levelsToCreate = metrics
-                .Select(
-                d => d.AuditionWordIdOpenCount.Select(d => d.Key)
-                .Union(d.CardsWordIdOpenCount.Select(d => d.Key))
-                // union with others levles types
-                .ToArray()).ToList();
+            metrics = await metrics.SaveCustomTraining(Context, userId).ConfigureAwait(false);
 
-            await Context.CreateLevels(userId, levelsToCreate).ConfigureAwait(false);
+            // calculate score
 
-            // calculate words stats
-            int[] metricsWordIds = metrics.SelectMany(m => m.CardsWordIdOpenCount.Keys).ToArray();
-            metricsWordIds = metricsWordIds.Union(metrics.SelectMany(m => m.AuditionWordIdOpenCount.Keys)).ToArray();
+            // calculate words metrics
 
-
-            // update words stats
-            var userWords = Context.UserWordPairs.Where(u => u.UserId == userId);
-            var existedWords = userWords.Where(d => metricsWordIds.Contains(d.UserWordPairId)).ToArray();
-
-            var dic2 = metrics.SelectMany(d => new AuditionQualifier(d.AuditionWordIdOpenCount).Qualify())
-                .GroupBy(kvp => kvp.Key)
-                .ToDictionary(g => g.Key, g => g.Max(x => x.Value));
-            
-            var dic1 = metrics.SelectMany(d => new CardGameQualifier(d.CardsWordIdOpenCount).Qualify())
-                .GroupBy(kvp => kvp.Key)
-                .ToDictionary(g => g.Key, g => g.Max(x => x.Value));
-
-            Dictionary<int, double> weight = new Dictionary<int, double>(metricsWordIds.Length);
-
-            foreach (var id in metricsWordIds)
-            {
-                double w = 0;
-                if (dic1.ContainsKey(id))
-                    w += 0.9;
-                if (dic2.ContainsKey(id))
-                    w += 0.1;
-                weight.Add(id, w * 0.8);
-            }
-            //existedWords.UpdateMemorisation(dic1.Union(dic2), 0.8);
-            await Context.SaveChangesAsync().ConfigureAwait(false);
-
-            // calculate card stars
-            // calculat audition stars
-
-            return await base.HandleRequest(request, cancellationToken);
+            return await base.HandleRequest(request, cancellationToken).ConfigureAwait(false);
         }
+
     }
 }
