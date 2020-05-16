7a655f7dc28eff431943e934cf571ce486b441b9
Alword <anzer987@yandex.ru>
2020-05-16 20:14:35 +0300

    feature path metric cropping

---
+++ WebApi/InWords.WebApi/InWords.Common/Extensions/StringExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InWords.Common.Extensions
+{
+    public static class StringExtensions
+    {
+        public static int IndexOfN(this string str, string value, int nth = 0)
+        {
+            if (nth < 0)
+                throw new ArgumentException("Can not find a negative index of substring in string. Must start with 0");
+
+            int offset = str.IndexOf(value);
+            for (int i = 0; i < nth; i++)
+            {
+                if (offset == -1) return -1;
+                offset = str.IndexOf(value, offset + 1);
+            }
+
+            return offset;
+        }
+    }
+}
