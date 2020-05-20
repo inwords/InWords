87a00ecdcffe8d0fc137e511bfca038d9a1d202c
Alword <anzer987@yandex.ru>
2020-05-20 13:37:54 +0300

    add games INW-136

---
+++ WebApi/InWords.WebApi/InWords.WebApi/Modules/WordsSets/GameEvaluator/Game/ClosedCardsLevel.cs
@@ -0,0 +1,17 @@
+﻿using InWords.Common.Extensions;
+using InWords.Data.Enums;
+using InWords.WebApi.Business.GameEvaluator.Enum;
+using InWords.WebApi.Business.GameEvaluator.Model;
+using InWords.WebApi.Modules.WordsSets.GameEvaluator.Game;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace InWords.WebApi.Business.GameEvaluator.Game
+{
+    public class ClosedCardsLevel : BaseClosedCardLevel
+    {
+        public ClosedCardsLevel(int gameLevelId, IDictionary<int, int> metrics)
+            : base(gameLevelId, metrics) { }
+        public override GameType Type => GameType.ClosedCards;
+    }
+}
