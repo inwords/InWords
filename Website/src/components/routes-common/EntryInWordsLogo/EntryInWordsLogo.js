e136b9e04fd6d4abb85f80a6d9f85cf3f7f18e03
Prometium <akrasnovskii@yandex.ru>
2020-05-25 18:49:45 +0300

    redesign sign-in / sign-up pages

--- Website/src/components/routes-common/EntryInWordsLogo/EntryInWordsLogo.js
+++ Website/src/components/routes-common/EntryInWordsLogo/EntryInWordsLogo.js
@@ -4,7 +4,7 @@ import InWordsLogo from 'src/components/routes-common/InWordsLogo';
 import './EntryInWordsLogo.css';
 
 function EntryInWordsLogo() {
-  return <InWordsLogo height="64" className="entry-in-words-logo" />;
+  return <InWordsLogo height="48" className="entry-in-words-logo" />;
 }
 
 export default EntryInWordsLogo;
