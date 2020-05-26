6f89d044bdb7e6604ae06b7638b246d9d83c719e
Alexander Krasnovsky <32463107+prometium@users.noreply.github.com>
2020-05-26 13:18:41 +0300

    some improvements
    * add openedAudioCards in courses trainings
    
    * improve cards animations, cleanup

--- Website/src/components/routes-common/AnimatedTrainingCard.js
+++ Website/src/components/routes-common/AnimatedTrainingCard.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import FadeAnimation from 'src/components/core/FadeAnimation';
-import Fade from 'src/components/core/Fade';
+import ZoomAnimation from 'src/components/core/ZoomAnimation';
 import TrainingCard from 'src/components/routes-common/TrainingCard';
 
-function AnimatedTrainingCard({ visible = true, ...rest }) {
+function AnimatedTrainingCard(props) {
   return (
-    <FadeAnimation>
-      <Fade in={visible}>
-        <div>
-          <TrainingCard {...rest} />
-        </div>
-      </Fade>
-    </FadeAnimation>
+    <ZoomAnimation>
+      <div>
+        <TrainingCard {...props} />
+      </div>
+    </ZoomAnimation>
   );
 }
-
-AnimatedTrainingCard.propTypes = {
-  visible: PropTypes.bool
-};
 
 export default AnimatedTrainingCard;
