8e401d4a90fd42e2719ff1d9e8e8107bff6f6287
rugged-bl <vasia.shumilov@gmail.com>
2020-05-27 19:15:48 +0300

    INW-22 core error handling classes

---
+++ Android/app/src/main/java/ru/inwords/inwords/core/error_handler/GrpcStatusMapper.kt
@@ -0,0 +1,34 @@
+package ru.inwords.inwords.core.error_handler
+
+import io.grpc.StatusRuntimeException
+import ru.inwords.inwords.core.grpc.GrpcStatus
+
+fun GrpcStatus.mapToNetworkRequestStatus(): NetworkRequestStatus {
+    return when (this) {
+        GrpcStatus.OK -> NetworkRequestStatus.OK
+        GrpcStatus.CANCELLED -> NetworkRequestStatus.CANCELLED
+        GrpcStatus.UNKNOWN -> NetworkRequestStatus.UNKNOWN
+        GrpcStatus.INVALID_ARGUMENT, GrpcStatus.OUT_OF_RANGE, GrpcStatus.FAILED_PRECONDITION -> NetworkRequestStatus.INVALID_ARGUMENT
+        GrpcStatus.ALREADY_EXISTS -> NetworkRequestStatus.ALREADY_EXISTS
+        GrpcStatus.PERMISSION_DENIED -> NetworkRequestStatus.PERMISSION_DENIED
+        GrpcStatus.RESOURCE_EXHAUSTED -> NetworkRequestStatus.RESOURCE_EXHAUSTED
+        GrpcStatus.UNIMPLEMENTED -> NetworkRequestStatus.UNIMPLEMENTED
+        GrpcStatus.NOT_FOUND -> NetworkRequestStatus.NOT_FOUND
+        GrpcStatus.INTERNAL, GrpcStatus.DATA_LOSS, GrpcStatus.ABORTED -> NetworkRequestStatus.INTERNAL
+        GrpcStatus.UNAVAILABLE, GrpcStatus.DEADLINE_EXCEEDED -> NetworkRequestStatus.LOW_CONNECTION
+        GrpcStatus.UNAUTHENTICATED -> NetworkRequestStatus.UNAUTHENTICATED
+    }
+}
+
+fun StatusRuntimeException.mapToNetworkException(): NetworkException {
+    val grpcStatus = GrpcStatus.from(status)
+    val mappedStatus = grpcStatus.mapToNetworkRequestStatus()
+
+    val description = status.description.orEmpty()
+    val message = "trailers: $trailers\noriginal status (${status.code}) description:$description"
+
+    return NetworkException(mappedStatus, description, message)
+}
+
+class NetworkException(val status: NetworkRequestStatus, val description: String, message: String) : RuntimeException(message)
+
