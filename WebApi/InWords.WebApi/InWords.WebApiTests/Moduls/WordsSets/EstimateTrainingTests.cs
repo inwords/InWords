e2d953e003f91c07dcac8b988d80ed2607213e6f
Alword <anzer987@yandex.ru>
2020-05-27 19:22:25 +0300

    add test that confirms saving the history once (fail)

--- WebApi/InWords.WebApi/InWords.WebApiTests/Moduls/WordsSets/EstimateTrainingTests.cs
+++ WebApi/InWords.WebApi/InWords.WebApiTests/Moduls/WordsSets/EstimateTrainingTests.cs
@@ -1,10 +1,12 @@
 ﻿using InWords.Data;
 using InWords.Data.Creations;
 using InWords.Data.Creations.GameBox;
+using InWords.Data.Enums;
 using InWords.Protobuf;
 using InWords.WebApi.Modules.WordsSets;
 using InWords.WebApi.Services.Abstractions;
 using InWords.WebApiTests.TestUtils;
+using Org.BouncyCastle.Ocsp;
 using System.Linq;
 using Xunit;
 using static InWords.Protobuf.TrainingDataRequest.Types;
@@ -31,7 +33,7 @@ namespace InWords.WebApiTests.Moduls.WordsSets
 
             // act
             TrainingDataRequest trainingDataRequest = new TrainingDataRequest();
-            Training training = new Training {};
+            Training training = new Training { };
             training.ClosedCardsMetric = new ClosedCardsMetric();
             training.ClosedCardsMetric.WordIdOpenCount.Add(1, 4);
             training.ClosedCardsMetric.WordIdOpenCount.Add(2, 4);
@@ -160,6 +162,78 @@ namespace InWords.WebApiTests.Moduls.WordsSets
             Assert.Single(result.Scores);
             Assert.Equal(6, result.Scores.Single().OpenedCards.Score);
             Assert.Equal(6, result.Scores.Single().Score);
+        }
+
+        [Fact]
+        public async void EstimageCustomGame_SaveOnce()
+        {
+            int userId = 1;
+            using InWordsDataContext context = InWordsDataContextFactory.Create();
+            await context.AddAccount(userId);
+            await context.SaveChangesAsync();
+
+            EstimateTraining handler = new EstimateTraining(context);
+
+            var payload = new TrainingDataRequest();
+
+            var training = new Training
+            {
+                AudioMetric = new AudioMetric()
+            };
+
+            training.AudioMetric.WordIdOpenCount.Add(1, 1);
+            training.AudioMetric.WordIdOpenCount.Add(2, 1);
+            training.AudioMetric.WordIdOpenCount.Add(3, 1);
+            payload.Metrics.Add(training);
+
+            var request = new AuthorizedRequestObject<TrainingDataRequest, TrainingScoreReply>(payload);
+
+            var result = await handler.HandleRequest(request);
+
+            Assert.Single(context.Games);
+            Assert.Single(context.GameTags);
+            Assert.Equal(GameTags.CustomLevelsHistory, context.GameTags.First().Tags);
+            Assert.Single(context.GameLevels);
+            Assert.Empty(context.GameLevelWords); // Because words 1,2,3 is not not found
+        }
+
+        [Fact]
+        public async void EstimageCustomGames_SaveOnce()
+        {
+            int userId = 1;
+            using InWordsDataContext context = InWordsDataContextFactory.Create();
+            await context.AddAccount(userId);
+            await context.SaveChangesAsync();
+
+            EstimateTraining handler = new EstimateTraining(context);
+
+            var payload = new TrainingDataRequest();
+
+            var training = new Training
+            {
+                AudioMetric = new AudioMetric(),
+                ClosedAudioCards2Metric = new ClosedAudioCardsTwoMetric()
+            };
+
+            training.AudioMetric.WordIdOpenCount.Add(1, 1);
+            training.AudioMetric.WordIdOpenCount.Add(2, 1);
+            training.AudioMetric.WordIdOpenCount.Add(3, 1);
+            payload.Metrics.Add(training);
+
+            training.ClosedAudioCards2Metric.WordIdOpenCount.Add(1, 1);
+            training.ClosedAudioCards2Metric.WordIdOpenCount.Add(2, 1);
+            training.ClosedAudioCards2Metric.WordIdOpenCount.Add(3, 1);
+            payload.Metrics.Add(training);
+
+            var request = new AuthorizedRequestObject<TrainingDataRequest, TrainingScoreReply>(payload);
+
+            var result = await handler.HandleRequest(request);
+
+            Assert.Single(context.Games);
+            Assert.Single(context.GameTags);
+            Assert.Equal(GameTags.CustomLevelsHistory, context.GameTags.First().Tags);
+            Assert.Single(context.GameLevels);
+            Assert.Empty(context.GameLevelWords); // Because words 1,2,3 is not not found
         }
     }
 }
