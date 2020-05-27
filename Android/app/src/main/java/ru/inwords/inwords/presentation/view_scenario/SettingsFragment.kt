674c1e232585873bc234d07cf5f49aa7e2e1a63f
rugged-bl <vasia.shumilov@gmail.com>
2020-05-27 15:09:33 +0300

    INW-144 add toolbar to SettingsFragment and hide navigation, small fixes

--- Android/app/src/main/java/ru/inwords/inwords/presentation/view_scenario/SettingsFragment.kt
+++ Android/app/src/main/java/ru/inwords/inwords/presentation/view_scenario/SettingsFragment.kt
@@ -1,14 +1,27 @@
 package ru.inwords.inwords.presentation.view_scenario
 
 import android.os.Bundle
-import androidx.preference.PreferenceFragmentCompat
-import androidx.preference.PreferenceManager
+import android.view.View
+import androidx.annotation.IdRes
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import ru.inwords.inwords.R
+import ru.inwords.inwords.databinding.FragmentSettingsToolbarBinding
 
+class SettingsFragment : Fragment(R.layout.fragment_settings_toolbar) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-class SettingsFragment : PreferenceFragmentCompat() {
-    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        addPreferencesFromResource(R.xml.fragment_settings)
-        PreferenceManager.setDefaultValues(requireContext(), R.xml.fragment_settings, false)
+        val binding = FragmentSettingsToolbarBinding.bind(view)
+        val navController = requireActivity().findNavController(R.id.main_nav_host_fragment)
+        binding.toolbar.setupWithNavController(navController)
     }
+
+    /**
+     * Workaround according to https://issuetracker.google.com/issues/142847973#comment8
+     */
+    private fun FragmentActivity.findNavController(@IdRes viewId: Int) =
+        (supportFragmentManager.findFragmentById(viewId) as NavHostFragment).navController
 }
