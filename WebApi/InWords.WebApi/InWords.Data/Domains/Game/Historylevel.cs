dc5fd034e8a8211017325f4cbf6d79f88c7b43ea
Alword <anzer987@yandex.ru>
2020-05-27 20:30:24 +0300

    add data annotation

--- WebApi/InWords.WebApi/InWords.Data/Domains/Game/Historylevel.cs
+++ WebApi/InWords.WebApi/InWords.Data/Domains/Game/Historylevel.cs
@@ -1,13 +1,14 @@
 ﻿using InWords.Data.Creations.GameBox;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace InWords.Data.Domains.Game
 {
     public class Historylevel
     {
-        public int GameLevelId { get; set; }
+        [ForeignKey(nameof(GameLevel))] public int GameLevelId { get; set; }
         public DateTime DateTime { get; set; }
         public int WordsCount { get; set; }
         public virtual GameLevel GameLevel { get; set; }
