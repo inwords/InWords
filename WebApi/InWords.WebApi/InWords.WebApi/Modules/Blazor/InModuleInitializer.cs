f2d9b3989834aecaaa1c6bbca18230ad86db1a52
Alword <anzer987@yandex.ru>
2020-06-01 16:28:28 +0300

    fix control panel

--- WebApi/InWords.WebApi/InWords.WebApi/Modules/Blazor/InModuleInitializer.cs
+++ WebApi/InWords.WebApi/InWords.WebApi/Modules/Blazor/InModuleInitializer.cs
@@ -2,6 +2,9 @@
 using InWords.WebApi.Module;
 using InWords.WebApi.Modules.DictionaryServiceHandler.Words;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace InWords.WebApi.Modules.Blazor
 {
@@ -9,11 +12,27 @@ namespace InWords.WebApi.Modules.Blazor
     {
         public override void ConfigureApp(IApplicationBuilder app)
         {
-            app.UseBlazorFrameworkFiles();
             app.UseEndpoints(endpoints =>
             {
-                //endpoints.MapDefaultControllerRoute();
-                endpoints.MapFallbackToFile("index.html");
+                endpoints.MapGet("/", context =>
+                {
+                    context.Response.Redirect("/control/");
+                    return context.Response.WriteAsync("Redirected");
+                });
+            });
+
+            app.MapWhen(ctx => ctx.Request.Path.StartsWithSegments("/control"), first =>
+            {
+                first.UseBlazorFrameworkFiles("/control");
+                first.UseStaticFiles();
+
+                first.UseRouting();
+                first.UseEndpoints(endpoints =>
+                {
+                    //endpoints.MapControllers();
+                    endpoints.MapDefaultControllerRoute();
+                    endpoints.MapFallbackToFile("/control/{*path:nonfile}", "control/index.html");
+                });
             });
         }
     }
--- WebApi/InWords.WebApi/InWords.WebApi/releasenotes.md
+++ WebApi/InWords.WebApi/InWords.WebApi/releasenotes.md
@@ -1,14 +1,4 @@
 ﻿### Release notes
 
-- [x] INW-147 fix history dubles
-- [x] INW-148 remove redundant pipeline artefacts
-- [x] INW-149 update default router
-
-### NEW experimental API
-- [ ] INW-132 all levels request
-
-* GRPC WordSets.v2.GetFullsets
-
-* REST GET WordSets.Fullsets
-
-Load all games with stars using pagination.
+- [x] INW-151 added default control route
+- [ ] INW-132 added all levels request with tests
