91b6c8e5415953339642153b94a295c8ae1d99b7
ASlepushko <ASlepushko@croc.ru>
2021-08-05 23:17:27 +0300

    remove ssl middleware

--- WebApi/InWords.WebApi/InWords.WebApi/AppStart/Program.cs
+++ WebApi/InWords.WebApi/InWords.WebApi/AppStart/Program.cs
@@ -26,6 +26,9 @@ namespace InWords.WebApi.AppStart
 				.UseServiceProviderFactory(new AutofacServiceProviderFactory())
 				.ConfigureWebHostDefaults(webHostBuilder =>
 				{
+
+					GetPorts(out int http1_2, out int http2);
+
 					string? path = AppDomain.CurrentDomain.BaseDirectory;
 					if (string.IsNullOrWhiteSpace(path)) path = "";
 
@@ -33,7 +36,15 @@ namespace InWords.WebApi.AppStart
 					.UseStartup<Startup>()
 					.UseKestrel((hostingContext, options) =>
 					{
-						options.Listen(IPAddress.Any, 8080);
+						options.Listen(IPAddress.Loopback, http1_2,
+							listenOptions => listenOptions.Protocols = HttpProtocols.Http1AndHttp2
+							);
+
+						options.Listen(IPAddress.Loopback, http2,
+							listenOptions =>
+							{
+								listenOptions.Protocols = HttpProtocols.Http2;
+							});
 					})
 					.UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration
 					.ReadFrom.Configuration(hostingContext.Configuration)
@@ -41,6 +52,22 @@ namespace InWords.WebApi.AppStart
 					.WriteTo.Console()
 					.WriteTo.File(Path.Combine(path, $"log/{DateTime.Now:yyyy-MM-dd-HH-mm}.txt")));
 				});
+		}
+
+		private static void GetPorts(out int http1_2, out int http2)
+		{
+			http1_2 = 5100;
+			http2 = 5101;
+
+			string? INWHTTP = Environment.GetEnvironmentVariable("INWHTTP");
+			string? INWHTTPS = Environment.GetEnvironmentVariable("INWHTTPS");
+
+			Console.WriteLine($"Environment {INWHTTP} {INWHTTPS}");
+
+			if (!string.IsNullOrWhiteSpace(INWHTTP))
+				http1_2 = int.Parse(INWHTTP, NumberFormatInfo.InvariantInfo);
+			if (!string.IsNullOrWhiteSpace(INWHTTPS))
+				http2 = int.Parse(INWHTTPS, NumberFormatInfo.InvariantInfo);
 		}
 	}
 }
